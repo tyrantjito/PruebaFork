cdeccd206e497884fd14ca4ab274305b1ba28d5e
Cesar B <cbautistac88@gmail.com>
2018-02-18 12:07:50 -0600

    Inicio de Proyecto, prueba fork

--- PruebaGIT_Fork/Form1.cs
+++ PruebaGIT_Fork/Form1.cs
@@ -16,5 +16,10 @@ namespace PruebaGIT_Fork
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Proyecto Original");
+        }
     }
 }
