c1c8e7337efec5bda342b372d3564bb987e2ba9a
Cesar Bautista <cbautistac88@gmail.com>
2018-02-18 12:19:40 -0600

    Realizando cambios en clon

--- PruebaGIT_Fork/Form1.cs
+++ PruebaGIT_Fork/Form1.cs
@@ -19,7 +19,7 @@ namespace PruebaGIT_Fork
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Proyecto Original");
+            MessageBox.Show("Proyecto Clonado para fork");
         }
     }
 }
